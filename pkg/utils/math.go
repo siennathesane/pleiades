f9bb179685f980c69f538337a5ac3fb4e4c23bc2
Sienna Lloyd <sienna.lloyd@hey.com>
2022-06-10 23:27:03 -0600

    added util func
    Signed-off-by: Sienna Lloyd <sienna.lloyd@hey.com>

--- pkg/utils/math.go
+++ pkg/utils/math.go
@@ -1,1 +1,12 @@
 package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomInt(min int, max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	p := r.Perm(max - min + 1)
+	return p[min]
+}
