31aed5bd9679e2fe467bb87d824e1c39f1e43508
Sienna Lloyd <sienna.lloyd@hey.com>
2022-06-10 23:26:32 -0600

    fixed generator.
    Signed-off-by: Sienna Lloyd <sienna.lloyd@hey.com>

--- gen.sh
+++ gen.sh
@@ -7,6 +7,7 @@ if ! command -v protoc  &> /dev/null; then
   echo "missing protoc"
 fi
 
+echo "Generating system protobufs"
 pb_files=()
 while IFS= read -r line; do
     pb_files+=( "$line" )
@@ -24,3 +25,19 @@ protoc \
   --go-vtproto_opt=features=all \
    --go-vtproto_opt=paths=source_relative \
   "${pb_files[@]}"
+
+
+echo "Generating test protobufs"
+
+protoc \
+  -I ./pkg/blaze/testdata \
+  --go_out=./pkg/blaze/testdata \
+  --go_opt=paths=source_relative \
+  --plugin protoc-gen-go-drpc="${GOBIN}/protoc-gen-go-drpc" \
+  --plugin protoc-gen-go-vtproto="${GOBIN}/protoc-gen-go-vtproto" \
+  --go-drpc_out=./pkg/blaze/testdata \
+  --go-drpc_opt=paths=source_relative \
+  --go-vtproto_out=./pkg/blaze/testdata \
+  --go-vtproto_opt=features=all \
+  --go-vtproto_opt=paths=source_relative \
+  ./pkg/blaze/testdata/cookie_monster.proto
