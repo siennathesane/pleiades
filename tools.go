5cbbd01b431dafb701d9320bc2f8275a07dfdd8c
Sienna Lloyd <sienna.lloyd@hey.com>
2022-06-30 13:29:36 -0600

    completely rid of GRPC!!!
    Signed-off-by: Sienna Lloyd <sienna.lloyd@hey.com>

--- tools.go
+++ tools.go
@@ -4,9 +4,4 @@ package tools
 
 import (
     _ "capnproto.org/go/capnp/v3/capnpc-go"
-    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-    _ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-    _ "google.golang.org/protobuf/cmd/protoc-gen-go"
-    _ "storj.io/drpc/cmd/protoc-gen-go-drpc"
 )
