39b0cb7c18452a544950151e3df15f81d1487b2d
Sienna Lloyd <sienna.lloyd@hey.com>
2022-06-30 21:24:18 -0600

    increased test server range for ci.
    Signed-off-by: Sienna Lloyd <sienna.lloyd@hey.com>

--- pkg/blaze/consts.go
+++ pkg/blaze/consts.go
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2022 Sienna Lloyd
+ *
+ * Licensed under the PolyForm Strict License 1.0.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License here:
+ *  https://github.com/mxplusb/pleiades/blob/mainline/LICENSE
+ */
+
 package blaze
 
 import (
@@ -6,8 +15,8 @@ import (
 )
 
 const (
-	testServerPortStart int = 8080
-	testServerPortStop  int = 8100
+	testServerPortStart int = 8000
+	testServerPortStop  int = 9000
 )
 
 func testServerAddr() string {
