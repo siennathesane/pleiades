df2897d3566f4b032ce6ce3e878b91248591ce41
Sienna Lloyd <sienna@linux.com>
2022-09-08 14:48:13 -0600

    added the ability to start replicas
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- pkg/server/raft_shard.go
+++ pkg/server/raft_shard.go
@@ -96,103 +96,6 @@ type shardManager struct {
 	nh     *dragonboat.NodeHost
 }
 
-func (c *shardManager) NewShard(shardId uint64, replicaId uint64, stateMachineType StateMachineType, timeout time.Duration) error {
-	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
-	l.Info().Msg("creating new shard")
-
-	clusterConfig := newDConfig(shardId, replicaId)
-
-	members := make(map[uint64]string)
-	members[replicaId] = c.nh.RaftAddress()
-	l.Debug().Str("raft-address", c.nh.RaftAddress()).Msg("adding self to members")
-
-	switch stateMachineType {
-	case testStateMachineType:
-		l.Info().Msg("creating test state machine")
-		return c.nh.StartCluster(members, false, newTestStateMachine, clusterConfig)
-	}
-
-	l.Error().Msg("unknown state machine type")
-	return ErrUnsupportedStateMachine
-}
-
-func (c *shardManager) GetLeaderId(shardId uint64) (leader uint64, ok bool, err error) {
-	c.logger.Info().Uint64("shard", shardId).Msg("getting leader id")
-	return c.nh.GetLeaderID(shardId)
-}
-
-func (c *shardManager) StopReplica(shardId uint64) (*OperationResult, error) {
-	c.logger.Info().Uint64("shard", shardId).Msg("stopping replica")
-	err := c.nh.StopCluster(shardId)
-	if err != nil {
-		c.logger.Error().Err(err).Uint64("shard", shardId).Msg("failed to stop replica")
-		return nil, err
-	}
-	return &OperationResult{}, nil
-}
-
-func (c *shardManager) RemoveReplica(shardId uint64, replicaId uint64, timeout time.Duration) error {
-	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
-	l.Info().Msg("deleting replica")
-
-	if timeout == 0 {
-		l.Debug().Msg("using default timeout")
-		timeout = defaultTimeout
-	}
-
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(timeout))
-	defer cancel()
-
-	members, err := c.GetShardMembers(shardId)
-	if err != nil {
-		l.Error().Err(err).Msg("failed to get shard members")
-		return err
-	}
-
-	if members.ConfigChangeId == 0 {
-		l.Error().Err(errNoConfigChangeId).Msg("failed to get config change id from shard members")
-		return errNoConfigChangeId
-	}
-
-	err = c.nh.SyncRequestDeleteNode(ctx, shardId, replicaId, members.ConfigChangeId)
-	if err != nil {
-		l.Error().Err(err).Msg("failed to delete replica")
-		return err
-	}
-	return nil
-}
-
-func (c *shardManager) GetShardMembers(shardId uint64) (*MembershipEntry, error) {
-	l := c.logger.With().Uint64("shard", shardId).Logger()
-	l.Debug().Msg("getting shard members")
-
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(defaultTimeout))
-	defer cancel()
-
-	membership, err := c.nh.SyncGetClusterMembership(ctx, shardId)
-	if err != nil {
-		l.Error().Err(err).Msg("failed to get shard membership")
-		return nil, err
-	}
-
-	return &MembershipEntry{
-		ConfigChangeId: membership.ConfigChangeID,
-		Replicas:       membership.Nodes,
-		Observers:      membership.Observers,
-		Witnesses:      membership.Witnesses,
-	}, nil
-}
-
-func (c *shardManager) RemoveData(shardId, replicaId uint64) error {
-	c.logger.Info().Uint64("shard", shardId).Uint64("replica", replicaId).Msg("removing data")
-
-	err := c.nh.RemoveData(shardId, replicaId)
-	if err != nil {
-		c.logger.Error().Err(err).Uint64("shard", shardId).Uint64("replica", replicaId).Msg("failed to remove data")
-	}
-	return err
-}
-
 func (c *shardManager) AddReplica(shardId uint64, replicaId uint64, newHost string, timeout time.Duration) error {
 	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
 	l.Info().Msg("adding replica")
@@ -261,6 +164,154 @@ func (c *shardManager) AddShardWitness(shardId uint64, replicaId uint64, newHost
 	err = c.nh.SyncRequestAddWitness(ctx, shardId, replicaId, newHost, members.ConfigChangeId)
 	if err != nil {
 		l.Error().Err(err).Msg("failed to add shard observer")
+	}
+	return err
+}
+
+func (c *shardManager) GetLeaderId(shardId uint64) (leader uint64, ok bool, err error) {
+	c.logger.Info().Uint64("shard", shardId).Msg("getting leader id")
+	return c.nh.GetLeaderID(shardId)
+}
+
+func (c *shardManager) GetShardMembers(shardId uint64) (*MembershipEntry, error) {
+	l := c.logger.With().Uint64("shard", shardId).Logger()
+	l.Debug().Msg("getting shard members")
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(defaultTimeout))
+	defer cancel()
+
+	membership, err := c.nh.SyncGetClusterMembership(ctx, shardId)
+	if err != nil {
+		l.Error().Err(err).Msg("failed to get shard membership")
+		return nil, err
+	}
+
+	return &MembershipEntry{
+		ConfigChangeId: membership.ConfigChangeID,
+		Replicas:       membership.Nodes,
+		Observers:      membership.Observers,
+		Witnesses:      membership.Witnesses,
+	}, nil
+}
+
+func (c *shardManager) NewShard(shardId uint64, replicaId uint64, stateMachineType StateMachineType, timeout time.Duration) error {
+	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
+	l.Info().Msg("creating new shard")
+
+	clusterConfig := newDConfig(shardId, replicaId)
+
+	members := make(map[uint64]string)
+	members[replicaId] = c.nh.RaftAddress()
+	l.Debug().Str("raft-address", c.nh.RaftAddress()).Msg("adding self to members")
+
+	switch stateMachineType {
+	case testStateMachineType:
+		l.Info().Msg("creating test state machine")
+		return c.nh.StartCluster(members, false, newTestStateMachine, clusterConfig)
+	}
+
+	l.Error().Msg("unknown state machine type")
+	return ErrUnsupportedStateMachine
+}
+
+func (c *shardManager) StartReplica(shardId uint64, replicaId uint64, stateMachineType StateMachineType) error {
+	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
+	l.Info().Msg("starting replica")
+
+	clusterConfig := newDConfig(shardId, replicaId)
+
+	switch stateMachineType {
+	case testStateMachineType:
+		l.Info().Msg("creating test state machine")
+		return c.nh.StartCluster(nil, true, newTestStateMachine, clusterConfig)
+	}
+
+	l.Error().Msg("unknown state machine type")
+	return ErrUnsupportedStateMachine
+}
+
+func (c *shardManager) StartReplicaObserver(shardId uint64, replicaId uint64, stateMachineType StateMachineType) error {
+	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
+	l.Info().Msg("starting replica observer")
+
+	clusterConfig := newDConfig(shardId, replicaId)
+	clusterConfig.IsObserver = true
+
+	switch stateMachineType {
+	case testStateMachineType:
+		l.Info().Msg("creating test state machine")
+		return c.nh.StartCluster(nil, true, newTestStateMachine, clusterConfig)
+	}
+
+	l.Error().Msg("unknown state machine type")
+	return ErrUnsupportedStateMachine
+}
+
+func (c *shardManager) StartReplicaWitness(shardId uint64, replicaId uint64, stateMachineType StateMachineType) error {
+	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
+	l.Info().Msg("starting replica witness")
+
+	clusterConfig := newDConfig(shardId, replicaId)
+	clusterConfig.IsWitness = true
+	clusterConfig.SnapshotEntries = 0
+
+	switch stateMachineType {
+	case testStateMachineType:
+		l.Info().Msg("creating test state machine")
+		return c.nh.StartCluster(nil, true, newTestStateMachine, clusterConfig)
+	}
+
+	l.Error().Msg("unknown state machine type")
+	return ErrUnsupportedStateMachine
+}
+
+func (c *shardManager) StopReplica(shardId uint64) (*OperationResult, error) {
+	c.logger.Info().Uint64("shard", shardId).Msg("stopping replica")
+	err := c.nh.StopCluster(shardId)
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("shard", shardId).Msg("failed to stop replica")
+		return nil, err
+	}
+	return &OperationResult{}, nil
+}
+
+func (c *shardManager) RemoveReplica(shardId uint64, replicaId uint64, timeout time.Duration) error {
+	l := c.logger.With().Uint64("shard", shardId).Uint64("replica", replicaId).Logger()
+	l.Info().Msg("deleting replica")
+
+	if timeout == 0 {
+		l.Debug().Msg("using default timeout")
+		timeout = defaultTimeout
+	}
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(timeout))
+	defer cancel()
+
+	members, err := c.GetShardMembers(shardId)
+	if err != nil {
+		l.Error().Err(err).Msg("failed to get shard members")
+		return err
+	}
+
+	if members.ConfigChangeId == 0 {
+		l.Error().Err(errNoConfigChangeId).Msg("failed to get config change id from shard members")
+		return errNoConfigChangeId
+	}
+
+	err = c.nh.SyncRequestDeleteNode(ctx, shardId, replicaId, members.ConfigChangeId)
+	if err != nil {
+		l.Error().Err(err).Msg("failed to delete replica")
+		return err
+	}
+	return nil
+}
+
+func (c *shardManager) RemoveData(shardId, replicaId uint64) error {
+	c.logger.Info().Uint64("shard", shardId).Uint64("replica", replicaId).Msg("removing data")
+
+	err := c.nh.RemoveData(shardId, replicaId)
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("shard", shardId).Uint64("replica", replicaId).Msg("failed to remove data")
 	}
 	return err
 }
