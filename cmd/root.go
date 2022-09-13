479be189bfb887ec755a4b9194d829133939479c
Sienna Lloyd <sienna@linux.com>
2022-09-13 14:17:11 -0600

    initial cli commit
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,9 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
+
+
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -33,15 +34,6 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
-		} else {
-			// Config file was found but another error was produced
-		}
-	}
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -53,13 +45,11 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "host", "", "host file (default is $HOME/.pleiades.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pleiades.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-	if err := viper.BindPFlags(rootCmd.Flags()); err != nil {
-		panic(err)
-	}
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
+
