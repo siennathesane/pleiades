88623b5715c8829e974c34c0cad4fa126dc07574
Sienna Lloyd <sienna@linux.com>
2022-09-13 12:58:05 -0600

    finish bbolt store.
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- pkg/server/raft_bbolt_manager.go
+++ pkg/server/raft_bbolt_manager.go
@@ -194,7 +194,7 @@ func (s *bboltStoreManager) CreateBucket(request *database.CreateBucketRequest) 
 	}
 
 	bucketName := request.GetName()
-	if owner == "" {
+	if bucketName == "" {
 		s.logger.Trace().Msg("empty bucket name")
 		return &database.CreateBucketReply{}, kv.ErrInvalidBucketName
 	}
@@ -273,7 +273,7 @@ func (s *bboltStoreManager) DeleteBucket(request *database.DeleteBucketRequest) 
 
 	req := &database.KVStoreWrapper{
 		Account: request.GetAccountId(),
-		Bucket: name,
+		Bucket:  name,
 		Typ:     database.KVStoreWrapper_DELETE_BUCKET_REQUEST,
 		Payload: &database.KVStoreWrapper_DeleteBucketRequest{
 			DeleteBucketRequest: request,
@@ -328,16 +328,217 @@ func (s *bboltStoreManager) DeleteBucket(request *database.DeleteBucketRequest) 
 }
 
 func (s *bboltStoreManager) GetKey(request *database.GetKeyRequest) (*database.GetKeyReply, error) {
-	//TODO implement me
-	panic("implement me")
+	account := request.GetAccountId()
+	if account == 0 {
+		s.logger.Trace().Msg("empty account value")
+		return &database.GetKeyReply{}, kv.ErrInvalidAccount
+	}
+
+	bucketName := request.GetBucketName()
+	if bucketName == "" {
+		s.logger.Trace().Msg("empty bucket name")
+		return &database.GetKeyReply{}, kv.ErrInvalidOwner
+	}
+
+	keyName := request.GetKey()
+	if keyName == "" {
+		s.logger.Trace().Msg("empty key name")
+		return &database.GetKeyReply{}, errors.New("empty key name")
+	}
+
+	req := &database.KVStoreWrapper{
+		Account: request.GetAccountId(),
+		Bucket:  bucketName,
+		Typ:     database.KVStoreWrapper_GET_KEY_REQUEST,
+		Payload: &database.KVStoreWrapper_GetKeyRequest{
+			GetKeyRequest: request,
+		},
+	}
+
+	cmd, err := req.MarshalVT()
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't marshal request")
+		return &database.GetKeyReply{}, errors.Wrap(err, "can't marshal request")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), utils.Timeout(s.defaultTimeout))
+	defer cancel()
+
+	shard := s.shardRouter.AccountToShard(account)
+
+	result, err := s.nh.SyncRead(ctx, shard, cmd)
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't apply message")
+		return &database.GetKeyReply{}, errors.Wrap(err, "can't apply message")
+	}
+
+	resp := &database.KVStoreWrapper{}
+	err = resp.UnmarshalVT(result.([]byte))
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't unmarshal response")
+		return &database.GetKeyReply{}, errors.Wrap(err, "can't unmarshal response")
+	}
+
+
+	response := &database.GetKeyReply{}
+	kvp := resp.GetGetKeyReply()
+	if kvp == nil {
+		response.KeyValuePair = &database.KeyValue{}
+	} else {
+		response.KeyValuePair = kvp.GetKeyValuePair()
+	}
+
+	return response, nil
 }
 
 func (s *bboltStoreManager) PutKey(request *database.PutKeyRequest) (*database.PutKeyReply, error) {
-	//TODO implement me
-	panic("implement me")
+	account := request.GetAccountId()
+	if account == 0 {
+		s.logger.Trace().Msg("empty account value")
+		return &database.PutKeyReply{}, kv.ErrInvalidAccount
+	}
+
+	bucketName := request.GetBucketName()
+	if bucketName == "" {
+		s.logger.Trace().Msg("empty bucketName value")
+		return &database.PutKeyReply{}, kv.ErrInvalidOwner
+	}
+
+	keyValuePair := request.GetKeyValuePair()
+	if keyValuePair == nil {
+		s.logger.Trace().Msg("empty key value pair")
+		return &database.PutKeyReply{}, kv.ErrInvalidBucketName
+	}
+
+	req := &database.KVStoreWrapper{
+		Account: request.GetAccountId(),
+		Bucket:  bucketName,
+		Typ:     database.KVStoreWrapper_PUT_KEY_REQUEST,
+		Payload: &database.KVStoreWrapper_PutKeyRequest{
+			PutKeyRequest: request,
+		},
+	}
+
+	cmd, err := req.MarshalVT()
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't marshal request")
+		return &database.PutKeyReply{}, errors.Wrap(err, "can't marshal request")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), utils.Timeout(s.defaultTimeout))
+	defer cancel()
+
+	var cs *client.Session
+	if request.Transaction != nil {
+		cs = s.tm.sessionCache[request.GetTransaction().GetClientId()]
+	} else {
+		shard := s.shardRouter.AccountToShard(account)
+		cs = s.nh.GetNoOPSession(shard)
+	}
+
+	result, err := s.nh.SyncPropose(ctx, cs, cmd)
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't apply message")
+		return &database.PutKeyReply{}, errors.Wrap(err, "can't apply message")
+	}
+
+	if cs.SeriesID != client.NoOPSeriesID {
+		cs.ProposalCompleted()
+	}
+
+	resp := &database.KVStoreWrapper{}
+	err = resp.UnmarshalVT(result.Data)
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't unmarshal response")
+		return &database.PutKeyReply{}, errors.Wrap(err, "can't unmarshal response")
+	}
+
+	response := &database.PutKeyReply{}
+
+	if request.Transaction == nil || cs.SeriesID != client.NoOPSeriesID {
+		response.Transaction = &database.Transaction{}
+	} else {
+		response.Transaction = csToTransaction(*cs)
+	}
+
+	return response, nil
 }
 
 func (s *bboltStoreManager) DeleteKey(request *database.DeleteKeyRequest) (*database.DeleteKeyReply, error) {
-	//TODO implement me
-	panic("implement me")
+	account := request.GetAccountId()
+	if account == 0 {
+		s.logger.Trace().Msg("empty account value")
+		return &database.DeleteKeyReply{}, kv.ErrInvalidAccount
+	}
+
+	bucketName := request.GetBucketName()
+	if bucketName == "" {
+		s.logger.Trace().Msg("empty bucket name")
+		return &database.DeleteKeyReply{}, kv.ErrInvalidOwner
+	}
+
+	key := request.GetKey()
+	if key == "" {
+		s.logger.Trace().Msg("empty key value pair")
+		return &database.DeleteKeyReply{}, kv.ErrInvalidBucketName
+	}
+
+	req := &database.KVStoreWrapper{
+		Account: request.GetAccountId(),
+		Bucket:  bucketName,
+		Typ:     database.KVStoreWrapper_DELETE_KEY_REQUEST,
+		Payload: &database.KVStoreWrapper_DeleteKeyRequest{
+			DeleteKeyRequest: request,
+		},
+	}
+
+	cmd, err := req.MarshalVT()
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't marshal request")
+		return &database.DeleteKeyReply{}, errors.Wrap(err, "can't marshal request")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), utils.Timeout(s.defaultTimeout))
+	defer cancel()
+
+	var cs *client.Session
+	if request.Transaction != nil {
+		cs = s.tm.sessionCache[request.GetTransaction().GetClientId()]
+	} else {
+		shard := s.shardRouter.AccountToShard(account)
+		cs = s.nh.GetNoOPSession(shard)
+	}
+
+	result, err := s.nh.SyncPropose(ctx, cs, cmd)
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't apply message")
+		return &database.DeleteKeyReply{}, errors.Wrap(err, "can't apply message")
+	}
+
+	if cs.SeriesID != client.NoOPSeriesID {
+		cs.ProposalCompleted()
+	}
+
+	resp := &database.KVStoreWrapper{}
+	err = resp.UnmarshalVT(result.Data)
+	if err != nil {
+		s.logger.Error().Err(err).Msg("can't unmarshal response")
+		return &database.DeleteKeyReply{}, errors.Wrap(err, "can't unmarshal response")
+	}
+
+	response := &database.DeleteKeyReply{}
+
+	if request.Transaction == nil || cs.SeriesID != client.NoOPSeriesID {
+		response.Transaction = &database.Transaction{}
+	} else {
+		response.Transaction = csToTransaction(*cs)
+	}
+
+	if resp.GetDeleteKeyReply() != nil {
+		response.Ok = resp.GetDeleteKeyReply().GetOk()
+	} else {
+		response.Ok = false
+	}
+
+	return response, nil
 }
