c479019d47ed7e40f361a5a57dee76f50d8741d3
Sienna Lloyd <sienna@linux.com>
2022-09-15 20:25:12 -0600

    fixed node package.
    Signed-off-by: Sienna Lloyd <sienna@linux.com>
    
    Changelog: added node client bindings

---
+++ pkg/server/server_pb.d.ts
@@ -0,0 +1,11 @@
+// package: server
+// file: pkg/server/server.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as jspb from "google-protobuf";
+import * as api_v1_database_kv_pb from "../../api/v1/database/kv_pb";
+import * as api_v1_database_transactions_pb from "../../api/v1/database/transactions_pb";
+import * as api_v1_raft_raft_shard_pb from "../../api/v1/raft/raft_shard_pb";
+import * as api_v1_raft_raft_host_pb from "../../api/v1/raft/raft_host_pb";
