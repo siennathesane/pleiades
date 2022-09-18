ee63fae166ef4981c0e101f59b23d710e1ea59d2
Sienna Lloyd <sienna@linux.com>
2022-09-17 21:43:01 -0600

    finished and tested the last phase
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- cmd/dev_bucket_create.go
+++ cmd/dev_bucket_create.go
@@ -10,16 +10,16 @@ package cmd
 
 import (
 	"context"
-	"time"
+	"net/http"
 
 	kvstorev1 "github.com/mxplusb/pleiades/pkg/api/kvstore/v1"
+	"github.com/mxplusb/pleiades/pkg/api/kvstore/v1/kvstorev1connect"
 	"github.com/mxplusb/pleiades/pkg/configuration"
-	"github.com/golang/protobuf/proto"
+	"github.com/bufbuild/connect-go"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 // bucketCreateCmd represents the bucketGet command
@@ -53,21 +53,12 @@ func bucketCreate(cmd *cobra.Command, args []string) {
 		logger.Fatal().Msg("account id cannot be zero")
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10000*time.Millisecond)
-	defer cancel()
+	client := kvstorev1connect.NewKvStoreServiceClient(http.DefaultClient, "http://localhost:8080")
 
-	conn, err := grpc.DialContext(ctx, serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	if err != nil {
-		logger.Fatal().Err(err).Msg("error dialing server")
-	}
-
-	client := kvstorev1.NewKvStoreServiceClient(conn)
-
-	logger.Info().Msg("creating bucket")
-	descriptor, err := client.CreateBucket(context.Background(), &kvstorev1.CreateBucketRequest{AccountId: accountId, Owner: accountOwner, Name: bucketName})
+	descriptor, err := client.CreateBucket(context.Background(), connect.NewRequest(&kvstorev1.CreateBucketRequest{AccountId: accountId, Owner: accountOwner, Name: bucketName}))
 	if err != nil {
 		logger.Fatal().Err(err).Msg("can't create bucket")
 	}
 
-	print(proto.MarshalTextString(descriptor))
+	print(protojson.Format(descriptor.Msg))
 }
