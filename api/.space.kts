e44804916c91aab9946499ce739b501291b68975
Sienna Lloyd <sienna@linux.com>
2022-12-01 15:50:05 -0800

    trying breaking with credentials
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/.space.kts
+++ api/.space.kts
@@ -30,7 +30,9 @@ job("lint") {
             interpreter = "/bin/bash"
             content = """
                 buf lint
-                buf breaking --against "ssh://git@git.jetbrains.space/anthroposlabs/pleiades/Pleiades.git#branch=mainline"
+                export BUF_INPUT_HTTPS_USERNAME=${'$'}JB_SPACE_CLIENT_ID
+                export BUF_INPUT_HTTPS_PASSWORD=${'$'}JB_SPACE_CLIENT_SECRET
+                buf breaking --against "https://git.jetbrains.space/anthroposlabs/pleiades/Pleiades.git#branch=mainline"
             """
         }
     }
