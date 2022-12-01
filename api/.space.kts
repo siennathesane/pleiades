8bba03e2e0085f364f2bb321fc5f247de5521812
Sienna Lloyd <sienna@linux.com>
2022-12-01 15:04:34 -0800

    updated gitignore and space def
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/.space.kts
+++ api/.space.kts
@@ -1,12 +1,13 @@
 job("Build CI Image") {
     kaniko {
-    build {
-        context = "docker"
-        dockerfile = "Dockerfile"
-    }
-    push("anthroposlabs.registry.jetbrains.space/p/pleiades/containers/api-ci") {
-        tags {
-            +"latest"
+        build {
+            context = "docker"
+            dockerfile = "Dockerfile"
+        }
+        push("anthroposlabs.registry.jetbrains.space/p/pleiades/containers/api-ci") {
+            tags {
+                +"latest"
+            }
         }
     }
 }
