65014e0eb805068399e0ce5802ae05f3449257b2
Sienna Lloyd <sienna@linux.com>
2022-12-01 20:05:07 -0800

    maybe the host key needs to be added...
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/ci/mirror.sh
+++ api/ci/mirror.sh
@@ -7,5 +7,7 @@ mkdir -p "$HOME"/.ssh
 echo "$DEPLOY_PRIVATE_KEY" > "$HOME"/.ssh/deploy
 export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy'
 
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
 git remote add mirror git@github.com:anthropos-labs/api.git
 git push --mirror mirror
