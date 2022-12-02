b3d41cc9f5efaba55a178cd0990f52e82d3830de
Sienna Lloyd <sienna@linux.com>
2022-12-01 20:06:56 -0800

    fixed permissions
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/ci/mirror.sh
+++ api/ci/mirror.sh
@@ -5,9 +5,10 @@ set -eux
 mkdir -p "$HOME"/.ssh
 
 echo "$DEPLOY_PRIVATE_KEY" > "$HOME"/.ssh/deploy
-export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy'
+chmod 0600 "$HOME"/.ssh/deploy
 
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
+export GIT_SSH_COMMAND='ssh -i ~/.ssh/deploy'
 git remote add mirror git@github.com:anthropos-labs/api.git
 git push --mirror mirror
