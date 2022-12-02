46de420f58ae4aa1cfc83ae7e15f41f356e830b2
Sienna Lloyd <sienna@linux.com>
2022-12-01 20:23:17 -0800

    fixed commands
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/ci/push.sh
+++ api/ci/push.sh
@@ -3,7 +3,7 @@
 echo "${BUF_API_TOKEN}" | buf registry login --username "${BUF_USER}" --token-stdin
 
 for f in databaseapi errorapi raftapi; do
-  pushd $f || exit
+  cd $f || exit
   buf push
-  popd || exit
+  cd .. || exit
 done
