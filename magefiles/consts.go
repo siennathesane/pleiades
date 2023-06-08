8127b01df3f781596a59ae5fc12d92576f70ddb8
Sienna Lloyd <sienna@linux.com>
2023-06-08 16:42:32 -0600

    updated license and vision
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- magefiles/consts.go
+++ magefiles/consts.go
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2023 Sienna Lloyd
+ *
+ * Licensed under the PolyForm Internal Use License 1.0.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License here:
+ *  https://github.com/mxplusb/pleiades/blob/mainline/LICENSE
+ */
+
 //go:build mage
 package main
 
--- magefiles/generate.go
+++ magefiles/generate.go
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2022 Sienna Lloyd
+ * Copyright (c) 2022-2023 Sienna Lloyd
  *
- * Licensed under the PolyForm Strict License 1.0.0 (the "License");
+ * Licensed under the PolyForm Internal Use License 1.0.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License here:
  *  https://github.com/mxplusb/pleiades/blob/mainline/LICENSE
