0284672d49dc6efede2cefb433998baaeab3e027
Sienna Lloyd <sienna@linux.com>
2023-06-08 00:41:25 -0600

    removed bad dir
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- api/ci/push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "${BUF_API_TOKEN}" | buf registry login --username "${BUF_USER}" --token-stdin
-
-for f in databaseapi errorapi raftapi; do
-  cd $f || exit
-  buf push
-  cd .. || exit
-done
